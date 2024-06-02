f56b95977c52bf75c921b0524683696e96e5726c
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2024-06-02 02:14:37 -0600

    9no
    Apariencia

--- obj/Debug/net7.0/Eats_Tech.AssemblyInfo.cs
+++ obj/Debug/net7.0/Eats_Tech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eats_Tech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca9b187bfdbb8daf37c6a0609ddd6860e884448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28c5334b84d9f7313a025ce5c1c1b54f3ce0ff3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eats_Tech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eats_Tech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
