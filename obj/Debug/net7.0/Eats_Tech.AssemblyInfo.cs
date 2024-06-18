6a8352eb0562b9b70e32fa8d3bda6397a3215784
AlexSAdeCV <alexacos@outlook.com>
2024-06-18 16:47:23 -0600

    cambios

--- obj/Debug/net7.0/Eats_Tech.AssemblyInfo.cs
+++ obj/Debug/net7.0/Eats_Tech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eats_Tech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8b17749e38b5b84c613d556973b5c79bc8c4c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e02adcc6c7cd812623598974da10a6036e74b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eats_Tech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eats_Tech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
